6e1873ab01d711de8ae4bda34ac2763023268e86
Victoria Argañaras <varganaras@cuentamono.com>
2024-12-20 10:03:59 -0300

    Add seeds

--- promo_market/priv/repo/seeds.exs
+++ promo_market/priv/repo/seeds.exs
@@ -1,11 +1,74 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     PromoMarket.Repo.insert!(%PromoMarket.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+# # Script for populating the database. You can run it as:
+# #
+# #     mix run priv/repo/seeds.exs
+
+alias PromoMarket.Catalog
+alias PromoMarket.Sales
+
+products = [
+  %{
+    code: "CF1",
+    name: "Arabica Coffee",
+    description: "Arabica Coffee beans 250 grams",
+    price: Money.new(11_23, :GBP),
+    stock: 45,
+    image_upload: "https://kantoxchallenge.s3.us-east-1.amazonaws.com/products-images/coffee.jpg"
+  },
+  %{
+    code: "SR1",
+    name: "Strawberry",
+    description: "Strawberry",
+    price: Money.new(5_00, :GBP),
+    stock: 324,
+    image_upload:
+      "https://kantoxchallenge.s3.us-east-1.amazonaws.com/products-images/strawberry.jpg"
+  },
+  %{
+    code: "GR1",
+    name: "Green Tea",
+    description: "Chinese Green Tea",
+    price: Money.new(3_11, :GBP),
+    stock: 61,
+    image_upload:
+      "https://kantoxchallenge.s3.us-east-1.amazonaws.com/products-images/green_tea.jpg"
+  }
+]
+
+[green_tea, strawberry, coffee] =
+  Enum.reduce(products, [], fn attrs, acc ->
+    {:ok, product} = Catalog.create_product(attrs)
+    List.insert_at(acc, 0, product)
+  end)
+
+expiration_date =
+  DateTime.utc_now()
+  |> DateTime.add(10, :day)
+
+promos = [
+  %{
+    name: "challenge_promo",
+    active: true,
+    discount_strategy: :buy_one_get_one_free,
+    expiration_date: expiration_date,
+    stock_limit: 200,
+    product_id: green_tea.id
+  },
+  %{
+    name: "challenge_promo",
+    active: true,
+    discount_strategy: :bulk_purchase_price_drop,
+    expiration_date: expiration_date,
+    stock_limit: 200,
+    product_id: strawberry.id
+  },
+  %{
+    name: "challenge_promo",
+    active: true,
+    discount_strategy: :bulk_purchase_price_drop,
+    expiration_date: expiration_date,
+    stock_limit: 200,
+    product_id: coffee.id
+  }
+]
+
+Enum.each(promos, fn attrs -> Sales.create_promo(attrs) end)
